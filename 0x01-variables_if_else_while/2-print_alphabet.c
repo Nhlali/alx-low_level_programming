2090878039346d67e085c8ddf4f2a8458f3bfda1
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-03-17 19:59:12 -0700

    0x01-variables_if_else_while 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*This program will assign a random number to the variable n each time it is
+*executed.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z' ; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
