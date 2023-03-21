1db2c2960a1366cb837764f2038eaa3bd7028f46
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-03-21 11:30:51 -0700

    0x02 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* print_alphabet - prints the alphabet, in lowercase,
+* followed by a new line.
+* 
 */
 void print_alphabet(void)
 {
