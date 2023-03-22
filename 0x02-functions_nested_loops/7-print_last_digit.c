71cd8f8739a134b812996026dec820d6393a1231
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-03-22 11:27:07 -0700

    0x02 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_last_digit - function that prints the last digit of a number.
+*
+* @n :  integer value.
+*
+* Return: positive 1, zero 0, negative -1.
+*
+*/
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
+int print_last_digit(int n)
+{
+	int v;
+	v = _abs(n);
+	_putchar('0' + (v % 10));
+	return (v % 10);
+}
