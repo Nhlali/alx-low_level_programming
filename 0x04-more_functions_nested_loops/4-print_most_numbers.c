23accee1a132291864376e0b05a898bc2b9c4f2e
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-03-24 03:07:12 -0700

    0x04 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_most_numbers -  function that prints the numbers, from 0 to 9,
+* followed by a new line, except 2 and 4.
+*
+*/
+void print_most_numbers(void)
+{
+	char c;
+	
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
