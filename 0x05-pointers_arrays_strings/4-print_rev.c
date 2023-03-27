60f8a1a0ee486a77ec57d15fabc0abb59da10e15
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-03-27 04:20:35 -0700

    0x05 4-print_rev.c£

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_rev - function that prints a string, in reverse,
+* followed by a new line.
+*
+* @s : pointer.
+*
+*/
+void print_rev(char *s)
+{
+	unsigned int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*s--);
+	}
+	_putchar('\n');
+}
