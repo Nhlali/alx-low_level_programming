28e29e8426810ea8d39a1de9a32fa0507dc2fb67
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-04-04 13:53:24 -0700

    0x06 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* reverse_array - reversing function.
+* @a: pointer
+* @n: pointer
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+	int b;
+
+	if (n % 2 != 0)
+	{
+		b = n + 1;
+	}
+	else
+	{
+		b = n;
+	}
+
+	for (i = 0; i < d / 2; i++)
+	{
+		j = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = j;
+	}
+}
