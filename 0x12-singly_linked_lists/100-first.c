71039cdbbd2b6284520b93935aa1101943ba4585
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-04-29 16:34:00 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+* before_main - a function that prints a string before the main function
+* is executed.
+*
+*/
+
 void before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
