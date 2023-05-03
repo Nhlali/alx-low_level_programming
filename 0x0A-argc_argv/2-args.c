69c976a6bbc36b788d0dc31c9df7b3e3323928fe
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-05-03 12:04:19 -0700

    taks 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - main function.
+* @argc : number of arguments.
+* @argv : array that contains the arguments.
+* Return: 0.
+*/
+ 
+int main(int argc, char *argv[])
+{
+
+	while (*argv)
+	{
+		(void) argc;
+		printf("%s\n", *argv);
+		argv++;
+	}
+	return (0);
+
+}
