d45159c4184cd87d299824df38e02084abb2afc4
Nhlalala Ngobeni <nqngobeni@gmail.com>
2023-05-03 11:57:05 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function.
+ * @argc : number of arguments.
+ * @argv : array that contains the arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+	
+}
